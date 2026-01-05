d6ae2f2997c15a0bb94606b5a78fa71e5e25b486
KaVamsiKrishna <kavamsi71@gmail.com>
2026-01-05 12:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	const radius = Number(e.target.radius.value);
+	const volumeEle = e.target.volume;
+	if(isNaN(radius) || radius <= 0) {
+		volumeEle.value = 'NaN'
+		return;
+	}
+	volumeEle.value = (4/3 * Math.PI * (radius * radius * radius)).toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
